f6f32e8ed48c20af4481db96d42d24a92a206233
Mahdi Sassi <m.sassi@root.sa>
2018-02-28 16:40:30 +0100

    add three properties (username, password, image)

--- ExampleListView/ExampleListView/Models/Employee.cs
+++ ExampleListView/ExampleListView/Models/Employee.cs
@@ -7,7 +7,10 @@ namespace ExampleListView.Models
    public class Employee
     {
         public String Name { get; set; }
+        public String UserName { get; set; }
+        public String PassWord { get; set; }
         public String Departement { get; set; }
+        public String image { get; set; }
         public Employee()
         {
 
